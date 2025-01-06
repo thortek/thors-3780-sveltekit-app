0833be9584b3da39381722547f3fb5f91ff886d8
thortek <ta.anderson@gmail.com>
2025-01-06 15:20:34 -0700

    Got the initial config going

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,22 @@
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
+import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: [typography, forms]
+	plugins: [
+		typography,
+		forms,
+		skeleton({
+			// NOTE: each theme included will increase the size of your CSS bundle
+			themes: [themes.cerberus, themes.rose]
+		})
+	]
 } satisfies Config;

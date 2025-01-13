01fd8e6b84a701017fe1e707b0559c92cf405d21
thortek <ta.anderson@gmail.com>
2025-01-13 15:01:39 -0700

    Improved the readiness check route to be Svelte 5 compatible

--- src/routes/mongodb/+page.server.ts
+++ src/routes/mongodb/+page.server.ts
@@ -1,13 +1,23 @@
-import clientPromise from "$lib/mongodb/mongodb.client";
+import clientPromise from '$lib/mongodb/mongodb.client';
+import type { PageServerLoad } from './$types';
 
-export async function load() {
-    const client = await clientPromise;
-    try {
-        await client?.db('sample_airbnb').command({ ping: 1 });
-        console.log('Pinged my MongoDB database.  It worked!');
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-    } finally {
-        await client?.close();
-    }
-}
+export const load: PageServerLoad = async ({ params }) => {
+	const client = await clientPromise;
+	try {
+		await client?.db('sample_airbnb').command({ ping: 1 });
+		console.log('Pinged my MongoDB database.  It worked!');
+	} catch (error) {
+		console.error('MongoDB connection error:', error);
+		return {
+			status: 500,
+			message: 'MongoDB connection error'
+		};
+	} finally {
+		await client?.close();
+	}
+
+	return {
+		status: 200,
+		message: 'MongoDB connection successful'
+	};
+};
--- src/routes/mongodb/+page.svelte
+++ src/routes/mongodb/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+ import type { PageData } from './$types'
 
+ let { data }: { data: PageData} = $props()
 </script>
 
 <h1>MongoDB connection page</h1>
+
+<p>Connection status: {data.status}</p>
+<p>Connection message: {data.message}</p>
